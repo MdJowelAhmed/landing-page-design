03b33f67293cdcb993f92c98b5db08bf734d2fc9
MdJowelAhmed <mdjowelahmed924@gmail.com>
2026-01-30 11:39:10 +0600

    update under hero section

--- src/components/home/HomeFeatureSection.tsx
+++ src/components/home/HomeFeatureSection.tsx
@@ -35,9 +35,11 @@ export default function HomeFeatureSection() {
 
                         <div className="md:flex justify-between w-96 md:w-[570px] items-center md:ml-20 gap-6 lg:gap-10 ">
                             {/* Bottom Stat */}
-                            <div className="flex flex-col mb-2  lg:mb-0">
-                                <h3 className="lg:text-4xl font-bold text-accent">42.0k+</h3>
-                                <p className="text-accent  text-sm">Employee Overview</p>
+                            <div className="hidden lg:block">
+                                <div className="flex flex-col mb-2  lg:mb-0">
+                                    <h3 className="lg:text-4xl font-bold text-accent">42.0k+</h3>
+                                    <p className="text-accent  text-sm">Employee Overview</p>
+                                </div>
                             </div>
 
                             {/* Text Content inside Dark Area */}
@@ -61,23 +63,35 @@ export default function HomeFeatureSection() {
                         {/* Dashboard Mockup Layer */}
                         <div className="relative z-10 p-4 md:pl-12 md:py-8 md:pr-0">
 
+                            <div className="absolute top-20 left-4 block lg:hidden  ">
+                                <div className="flex flex-col mb-2  lg:mb-0 ">
+                                    <h3 className="lg:text-4xl font-bold text-accent">42.0k+</h3>
+                                    <p className="text-accent  text-sm">Employee Overview</p>
+                                </div>
+                            </div>
 
                             {/* Dashboard Image Mockup */}
-                           <div className="  ">
-                           <Image src="/assets/dashboard.png" alt="Dashboard" width={800} height={200}  className="w-full h-full object-cover" />
-                           </div>
+                            <div className="  ">
+                                <Image src="/assets/dashboard.png" alt="Dashboard" width={800} height={200} className="w-full h-full object-cover" />
+                            </div>
                             {/* CTA Button "Cutout" */}
 
                             <Button
                                 size="lg"
-                                className="bg-gradient-to-r from-primary to-primary-foreground text-white px-5 h-12 font-bold rounded-lg shadow-lg absolute bottom-12 right-12  md:-right-2"
+                                className=" hidden lg:block bg-gradient-to-r from-primary to-primary-foreground text-white px-4 lg:px-5 py-3 lg:py-6 lg:h-12 h-10 font-bold rounded-lg shadow-lg absolute bottom-12 right-12  md:-right-2"
                             >
                                 Start Your Free Trial
                             </Button>
-                        
+
+                            <div className=" block lg:hidden absolute bottom-4 right-2 text-white md:-right-2">
+                                <Button className="bg-gradient-to-r from-primary to-primary-foreground text-white px-4 lg:px-5 py-2 lg:py-6 lg:h-12 h-8 font- rounded-lg shadow-lg">
+                                    Free Trial
+                                </Button>
+                            </div>
+
                         </div>
 
-                        
+
                     </div>
                 </div>
             </div>
